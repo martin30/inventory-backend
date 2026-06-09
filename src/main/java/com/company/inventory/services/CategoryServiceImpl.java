10a22b8314aa001c2391885612b1e5001b6c7797
Martin30 <martinmarquinag@gmail.com>
2026-06-09 12:56:42 -0500

    Servicio guardar categoria

--- src/main/java/com/company/inventory/services/CategoryServiceImpl.java
+++ src/main/java/com/company/inventory/services/CategoryServiceImpl.java
@@ -69,5 +69,32 @@ public class CategoryServiceImpl implements ICategoryService{
 			return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
+	}
+
+	@Override
+	@Transactional()
+	public ResponseEntity<CategoryResponseRest> save(Category category) {
+
+
+		CategoryResponseRest response = new CategoryResponseRest();
+		List<Category> list = new ArrayList<>();
+		
+		try {
+			Category categorySaved= categoryDao.save(category);
+			if(categorySaved !=null) {
+				list.add(categorySaved);
+				response.getCategoryResponse().setCategory(list);
+				response.setMetadata("espuesta Ok", "00", "Categoria guardada");
+			}else {
+				response.setMetadata("Respuesta no","-1","Ctegoria no guardada");
+				return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.BAD_REQUEST);
+			}
+		}catch (Exception e) {
+			response.setMetadata("Respuesta no","-1","Error al grabar categoria");
+			e.getStackTrace();
+			return  new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+			
+		}
+		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
 	}	
 }
--- src/main/java/com/company/inventory/services/ICategoryService.java
+++ src/main/java/com/company/inventory/services/ICategoryService.java
@@ -2,11 +2,13 @@ package com.company.inventory.services;
 
 import org.springframework.http.ResponseEntity;
 
+import com.company.inventory.model.Category;
 import com.company.inventory.response.CategoryResponseRest;
 
 public interface ICategoryService {
 
 	public ResponseEntity<CategoryResponseRest> search();
 	public ResponseEntity<CategoryResponseRest> searchbById(Long Id);
+	public ResponseEntity<CategoryResponseRest> save(Category category);
 		
 }
